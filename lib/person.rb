abe46bf13dceaa3e2b75a2258386d83f2ded4d7a
Olivia McGoffin <olivia@thoughtbot.com>
2020-01-14 16:05:21 -0800

    Added a PhoneNumberFormatValidator and tests to follow. Please note that for things to process as 'not valid', they need to not just return false, but instead pipe errors

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,8 @@
+require 'phone_number_format_validator'
+
 class Person
   include ActiveModel::Validations
-  PHONE_REGEX = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/
-
-  validates :phone_number, format: { with: PHONE_REGEX, multiline: true, message: 'invalid phone number formatting' }
-
+  validates :phone_number, presence: true, phone_number_format: true
   attr_accessor :phone_number
 
   def initialize(attributes = {})
